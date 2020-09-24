86c29993e7ea0e7c621781aa487d6e25cb9ddba7
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-09-24 13:30:22 -0400

    linear search function

---
+++ script.js
@@ -0,0 +1,12 @@
+const data = [12, 23, 38, 40, 54, 62, 71, 87, 99];
+
+function findIndex(num) {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i] === num) {
+      console.log(`${num} found at index ${i}`);
+      return;
+    }
+  }
+}
+
+findIndex(99);
